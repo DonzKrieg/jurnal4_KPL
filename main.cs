fb7adf366f7097cf7236dd5947e347bec74cf164
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 06:46:04 +0700

    first commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+﻿namespace modul4_103022330150
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
