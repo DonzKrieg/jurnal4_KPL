a3766f0eb861e0fc191cf8eebbac2221331d16c8
DonzKrieg <iromdhon2004@gmail.com>
2025-03-11 07:12:29 +0700

    first commit

---
+++ KodeProduk.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022330150
+{
+    internal class KodeProduk
+    {
+        private static readonly Dictionary<string, string> dataKodeProduk = new Dictionary<string, string>
+        {
+            {"Laptop", "E100" },
+            {"Smartphone", "E101" },
+            {"Tablet", "E102" },
+            {"Headset", "E103" },
+            {"Keyboard", "E104" },
+            {"Mouse", "E105" },
+            {"Printer", "E106" },
+            {"Monitor", "E107" },
+            {"Smartwatch", "E108" },
+            {"Kamera", "E109" }
+        };
+
+        public string getKodeProduk(string produk)
+        {
+            if (dataKodeProduk.ContainsKey(produk))
+            {
+                return dataKodeProduk[produk];
+            }
+
+            return "Kode produk tidak ditemukan!";
+        }
+    }
+}
--- main.cs
+++ main.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            KodeProduk kodeProduk = new KodeProduk();
+
+            Console.WriteLine("Masukkan nama produk:");
+            string produk = Console.ReadLine();
+            Console.WriteLine("Kode Produk " + produk + "-> " + kodeProduk.getKodeProduk(produk));
         }
     }
 }
